c1723edb42d009c3ccb7c9ea3bd3828ae6c9ce9d
eduardo ciudad figueredo <eduardociudadfigueredo@gmail.com>
2026-06-06 10:29:58 -0300

    feat: create dto input cartegoryInput

---
+++ src/main/java/com/eduardo/ecomerce/dto/input/category/CategoryInput.java
@@ -0,0 +1,11 @@
+package com.eduardo.ecomerce.dto.input.category;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record CategoryInput(
+        @NotBlank(message = "Nome é obrigatório")
+        @Size(max = 100, message = "Nome deve ter no máximo 100 caracteres")
+        String namegit a
+) {
+}
