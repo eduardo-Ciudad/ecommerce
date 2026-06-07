9c994596584dbe2d616b1147f108de6e41e26360
eduardo ciudad figueredo <eduardociudadfigueredo@gmail.com>
2026-06-07 14:38:37 -0300

    fix: now user imlements UserDetails

--- src/main/java/com/eduardo/ecomerce/domain/user/User.java
+++ src/main/java/com/eduardo/ecomerce/domain/user/User.java
@@ -2,8 +2,13 @@ package com.eduardo.ecomerce.domain.user;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -11,7 +16,7 @@ import java.util.UUID;
 @Getter
 @Setter
 @NoArgsConstructor
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -36,5 +41,15 @@ public class User {
     @PrePersist
     private void prePersist() {
         this.createdAt = LocalDateTime.now();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
     }
 }
