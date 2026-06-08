1a9dd56f4b1dc9d44d546ddc5af9ff611fdd847f
eduardo ciudad figueredo <eduardociudadfigueredo@gmail.com>
2026-06-07 21:08:32 -0300

    feat: create dto registerInput

---
+++ src/main/java/com/eduardo/ecomerce/dto/input/register/RegisterInput.java
@@ -0,0 +1,20 @@
+package com.eduardo.ecomerce.dto.input.register;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record RegisterInput(
+
+        @NotBlank(message = "Nome é obrigatório")
+        String name,
+
+        @NotBlank(message = "Email é obrigatório")
+        @Email(message = "Email inválido")
+        String email,
+
+        @NotBlank(message = "Senha é obrigatória")
+        @Size(min = 6, message = "Senha deve ter no mínimo 6 caracteres")
+        String password
+) {}
