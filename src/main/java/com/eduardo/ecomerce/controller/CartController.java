6287de01ce22c5e5016c9e0d0be1f8dde40a251f
eduardo ciudad figueredo <eduardociudadfigueredo@gmail.com>
2026-06-08 21:50:16 -0300

    fix: add aythentication do security utils no cartController

--- src/main/java/com/eduardo/ecomerce/controller/CartController.java
+++ src/main/java/com/eduardo/ecomerce/controller/CartController.java
@@ -2,6 +2,7 @@ package com.eduardo.ecomerce.controller;
 
 import com.eduardo.ecomerce.dto.input.cartitem.CartItemInput;
 import com.eduardo.ecomerce.dto.output.cart.CartOutput;
+import com.eduardo.ecomerce.infra.security.SecurityUtils;
 import com.eduardo.ecomerce.service.CartService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -10,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
-
 @RestController
 @RequestMapping("/cart")
 @RequiredArgsConstructor
@@ -18,21 +18,21 @@ public class CartController {
 
     private final CartService cartService;
 
-    @GetMapping("/{userId}")
-    public ResponseEntity<CartOutput> findByUserId(@PathVariable UUID userId) {
+    @GetMapping
+    public ResponseEntity<CartOutput> getCart() {
+        UUID userId = SecurityUtils.getAuthenticatedUserId();
         return ResponseEntity.ok(cartService.findByUserId(userId));
     }
 
-    @PostMapping("/{userId}/items")
-    public ResponseEntity<CartOutput> addItem(
-            @PathVariable UUID userId,
-            @RequestBody @Valid CartItemInput input) {
+    @PostMapping("/items")
+    public ResponseEntity<CartOutput> addItem(@RequestBody @Valid CartItemInput input) {
+        UUID userId = SecurityUtils.getAuthenticatedUserId();
         return ResponseEntity.status(HttpStatus.CREATED).body(cartService.addItem(userId, input));
     }
 
     @PutMapping("/items/{id}")
-    public ResponseEntity<CartOutput> updateItem(@PathVariable UUID id, @RequestBody Integer quantity) {
-        return ResponseEntity.ok(cartService.updateItem(id, quantity));
+    public ResponseEntity<CartOutput> updateItem(@PathVariable UUID id, @RequestBody @Valid CartItemInput input) {
+        return ResponseEntity.ok(cartService.updateItem(id, input.quantity()));
     }
 
     @DeleteMapping("/items/{id}")
