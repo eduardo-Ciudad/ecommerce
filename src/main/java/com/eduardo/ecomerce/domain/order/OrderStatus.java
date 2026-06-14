4dd46a53c0bf01297bd0bbab2c35e0ea1dae449a
eduardo ciudad figueredo <eduardociudadfigueredo@gmail.com>
2026-06-14 11:37:18 -0300

    feat: add sistema de verificacao dos enums

--- src/main/java/com/eduardo/ecomerce/domain/order/OrderStatus.java
+++ src/main/java/com/eduardo/ecomerce/domain/order/OrderStatus.java
@@ -1,5 +1,25 @@
 package com.eduardo.ecomerce.domain.order;
 
+import java.util.Set;
+
 public enum OrderStatus {
-    PENDING, PAID, SHIPPED, DELIVERED, CANCELLED
+    PENDING,
+    PAID,
+    SHIPPED,
+    DELIVERED,
+    CANCELLED;
+
+    private Set<OrderStatus> allowedStatuses;
+
+    static  {
+        PENDING.allowedStatuses = Set.of(PAID, CANCELLED);
+        PAID.allowedStatuses = Set.of(SHIPPED, CANCELLED);
+        SHIPPED.allowedStatuses = Set.of(DELIVERED);
+        DELIVERED.allowedStatuses = Set.of();
+        CANCELLED.allowedStatuses = Set.of();
+    }
+
+    public boolean canTransition(OrderStatus target) {
+        return allowedStatuses.contains(target);
+    }
 }
