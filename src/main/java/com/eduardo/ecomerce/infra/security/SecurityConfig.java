db135fde1b488dda86bbb66770e4232bf1775202
eduardo ciudad figueredo <eduardociudadfigueredo@gmail.com>
2026-07-16 18:50:02 -0300

    feat: mudei a url do front

--- src/main/java/com/eduardo/ecomerce/infra/security/SecurityConfig.java
+++ src/main/java/com/eduardo/ecomerce/infra/security/SecurityConfig.java
@@ -33,10 +33,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(request -> {
                     var config = new org.springframework.web.cors.CorsConfiguration();
                     config.setAllowedOrigins(java.util.List.of(
-                            "http://localhost:5173",
-                            "http://localhost:5174",
-                            "http://localhost:5175",
-                            "http://localhost:5176"
+                            "http://127.0.0.1:5500"
                     ));
                     config.setAllowedMethods(java.util.List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                     config.setAllowedHeaders(java.util.List.of("*"));
