8e843d8ee5baf9b99217c2a2414d1770d1aa39be
eduardo ciudad figueredo <eduardociudadfigueredo@gmail.com>
2026-07-19 12:51:48 -0300

    feat: liberei a rota do mercado pago

--- src/main/java/com/eduardo/ecomerce/infra/security/SecurityConfig.java
+++ src/main/java/com/eduardo/ecomerce/infra/security/SecurityConfig.java
@@ -49,6 +49,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/categories/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html", "/v3/api-docs").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/payments/webhook").permitAll()
                         // rotas de admin
                         .requestMatchers(HttpMethod.POST, "/products/**").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.PUT, "/products/**").hasRole("ADMIN")
