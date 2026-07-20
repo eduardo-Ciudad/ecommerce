87655ecba5cab040fbe583d9f365f78bbc0b679d
eduardo ciudad figueredo <eduardociudadfigueredo@gmail.com>
2026-07-20 17:11:31 -0300

    feat: liberei o cors

--- src/main/java/com/eduardo/ecomerce/infra/security/SecurityConfig.java
+++ src/main/java/com/eduardo/ecomerce/infra/security/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html", "/v3/api-docs").permitAll().requestMatchers(HttpMethod.POST, "/payments/webhook").permitAll()
                         .requestMatchers(HttpMethod.POST, "/payments/webhook").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/payments/checkout/**").authenticated()
+                        .requestMatchers(HttpMethod.POST, "/payments/process").authenticated()
 
                         // rotas de admin
                         .requestMatchers(HttpMethod.POST, "/products/**").hasRole("ADMIN")
