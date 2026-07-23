2636169b059e1e80bfdcc102725cfaff1e710810
eduardo ciudad figueredo <eduardociudadfigueredo@gmail.com>
2026-07-23 15:41:01 -0300

    feat: create local enviromensts no aplication properties

--- src/main/java/com/eduardo/ecomerce/EcomerceApplication.java
+++ src/main/java/com/eduardo/ecomerce/EcomerceApplication.java
@@ -2,8 +2,10 @@ package com.eduardo.ecomerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class EcomerceApplication {
 
 	public static void main(String[] args) {
