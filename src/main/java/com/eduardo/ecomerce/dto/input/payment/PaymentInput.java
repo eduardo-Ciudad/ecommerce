832b4240df14e30e671de667f1f36ca284295151
eduardo ciudad figueredo <eduardociudadfigueredo@gmail.com>
2026-07-27 14:30:59 -0300

    feat: add @Pattern no paymentMethod

--- src/main/java/com/eduardo/ecomerce/dto/input/payment/PaymentInput.java
+++ src/main/java/com/eduardo/ecomerce/dto/input/payment/PaymentInput.java
@@ -2,12 +2,17 @@ package com.eduardo.ecomerce.dto.input.payment;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 import java.util.UUID;
 
 public record PaymentInput(
         @NotNull UUID orderId,
-        @NotBlank String paymentMethod,
+
+
+        @NotBlank
+        @Pattern(regexp = "^(credit_card|pix)$", message = "Método de pagamento deve ser 'credit_card' ou 'pix'")
+        String paymentMethod,
         String token,
         Integer installments,
         String cardIssuerId
